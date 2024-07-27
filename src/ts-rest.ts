5e4a8846ee4d517162f5bab67ad8a562c4a06ffe
guillaume-docquier-vention <guillaume.docquier@vention.cc>
2024-07-26 22:29:48 -0400

    Added @types/node since it has the fetch and AbortSignal types

--- src/ts-rest.ts
+++ src/ts-rest.ts
@@ -46,3 +46,19 @@ const client = initClient(contract, {
 client.auth.login("invalid") // Problem: type of "login" is "any"
 client.logout("invalid") // Problem: type of "logout" is "any"
 client.invalid() // As expected: TS2339: Property invalid does not exist on type
+
+/**
+ * The following is to showcase that fetch and AbortSignal types are included in @types/node >=20
+ */
+async function showcaseFetchAndAbortSignalTypes() {
+  const controller: AbortController = new AbortController();
+  const signal: AbortSignal = controller.signal;
+
+  const request: Request = new Request("https://example.org/get", { signal });
+  const response: Response = await fetch(request);
+
+  controller.abort();
+
+  const text = await response.text();
+  console.log(text);
+}
